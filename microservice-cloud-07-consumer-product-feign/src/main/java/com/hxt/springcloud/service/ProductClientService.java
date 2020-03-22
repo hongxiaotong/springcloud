fc62488384b9d6384fa3e54fb08c3621e97c2057
hongxiaotong <706614700@qq.com>
2020-03-22 23:13:10 +0800

    Feign改造RestTemplate+Ribbon实现远程调用和负载均衡

---
+++ microservice-cloud-07-consumer-product-feign/src/main/java/com/hxt/springcloud/service/ProductClientService.java
@@ -0,0 +1,21 @@
+package com.hxt.springcloud.service;
+
+import com.hxt.springcloud.entities.Product;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@FeignClient(value = "microservice-product")
+public interface ProductClientService {
+    @RequestMapping(value = "/product/get/{id}", method = RequestMethod.GET)
+    Product get(@PathVariable("id") Long id);
+
+    @RequestMapping(value = "/product/list", method = RequestMethod.GET)
+    List<Product> list();
+
+    @RequestMapping(value = "/product/add", method = RequestMethod.POST)
+    boolean add(Product product);
+}
