a2e1ca3bb59d2eb2cf022eb2307a4bc8840b070b
hongxiaotong <706614700@qq.com>
2020-03-25 23:47:06 +0800

    配置中心

---
+++ microservice-cloud-10-zuul-gateway-7001/src/main/java/com/hxt/springcloud/ZuulServer_7001.java
@@ -0,0 +1,15 @@
+package com.hxt.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulServer_7001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulServer_7001.class,args);
+    }
+}
---
+++ microservice-cloud-11-config-server-5001/src/main/java/com/hxt/springcloud/ConfigServer_5001.java
@@ -0,0 +1,14 @@
+package com.hxt.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServer_5001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServer_5001.class,args);
+    }
+}
