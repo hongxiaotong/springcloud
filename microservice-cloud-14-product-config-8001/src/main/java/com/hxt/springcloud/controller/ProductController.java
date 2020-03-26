4db0e77b995de55a02d038776a040cbd070b6ab2
hongxiaotong <706614700@qq.com>
2020-03-26 23:44:47 +0800

    消息总线

--- microservice-cloud-14-product-config-8001/src/main/java/com/hxt/springcloud/controller/ProductController.java
+++ microservice-cloud-14-product-config-8001/src/main/java/com/hxt/springcloud/controller/ProductController.java
@@ -3,12 +3,23 @@ package com.hxt.springcloud.controller;
 import com.hxt.springcloud.entities.Product;
 import com.hxt.springcloud.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RefreshScope
 @RestController
 public class ProductController {
+
+    @Value("${emp.name}")
+    private String name;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return name;
+    }
 
     @Autowired
     private ProductService productService;
